afe113fc10114646e4ff29f0e1c79b308b1c3a2e
ndeberuti <ndeberuti@gmail.com>
2021-02-20 13:40:13 -0300

    Rename endpoint to clientMigrationStatus and delegate agreggation to the database engine

--- src/server/controller/clientStatusController.js
+++ src/server/controller/clientStatusController.js
@@ -1,6 +1,6 @@
 const sql = require("mssql");
 
-const clientStatusController = async (req, res, next) => {
+const clientMigrationStatusController = async (req, res, next) => {
     try {
         console.log('ClientId: ', req.params.id)
         // config for your database
@@ -12,24 +12,21 @@ const clientStatusController = async (req, res, next) => {
         };
     
         // connect to your database
-        sql.connect(config, function (err) {
-            
+        sql.connect(config, function(err) {
             if (err) console.log(err);
     
             // create Request object
             const request = new sql.Request();
                
             // query to the database and get the records
-            request.query(`select * from events where clientId = ${req.params.id}`, function (err, recordset) {
+            request.query(`select top 1 status from events where clientId = ${req.params.id} and eventType = 'MIGRATION' order by timestamp desc`, function (err, recordset) {
                 
                 if (err) console.log(err);
-    
-                // send records as a response
+        
+                // send normalized records as a response
                 res.send(processClientStatus(recordset));
-                
-            });
+             });
         });
-  
      
     } catch (error) {
       next(error);
@@ -37,9 +34,10 @@ const clientStatusController = async (req, res, next) => {
   }
 
 const processClientStatus = (recordset) => {
-    return recordset.recordset.filter(record => record.eventType = 'MIGRATION').reduce((a,b) => (a.timestamp > b.timestamp ? a : b)).status;
+    return recordset.recordset[0];
 }
+
 module.exports={
-    clientStatusController
+    clientMigrationStatusController
   }
   
--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { clientStatusController } = require('./controller/clientStatusController');
+const { clientMigrationStatus } = require('./controller/clientStatusController');
 const app = express();
 const port = 3000;
 
 
-app.get('/clientStatus/:id', clientStatusController);
+app.get('/clientMigrationStatus/:id', clientMigrationStatus);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
